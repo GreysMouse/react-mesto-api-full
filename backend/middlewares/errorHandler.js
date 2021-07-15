8981a49f45b57f2374c46205c6e303692efa9440
Diana Kondakova <greys.mouse@yandex.ru>
2021-07-15 19:12:11 +0300

    sprint 15 iteration (1): removing celebrate default errrs() mdwr

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,17 +1,13 @@
 /* eslint-disable no-unused-vars */
-// const { isCelebrateError } = require('celebrate');
+const { isCelebrateError } = require('celebrate');
 
 const errorHandler = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
-  res.status(statusCode).send({
+  if (isCelebrateError(err)) res.status(statusCode).send({ message });
+  else res.status(statusCode).send({
     message: statusCode === 500 ? 'Server error' : message,
   });
-
-  // if (isCelebrateError(err)) res.status(statusCode).send({ message });
-  // else res.status(statusCode).send({
-  //   message: statusCode === 500 ? 'Server error' : message,
-  // });
 };
 
 module.exports = { errorHandler };
