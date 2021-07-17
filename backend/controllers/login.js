74ddec012d70fd002828018b9170ca3f9db7b05e
Diana Kondakova <greys.mouse@yandex.ru>
2021-07-17 20:43:36 +0300

    removing samesite and httpOnly attribute in cookies

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -22,7 +22,7 @@ const login = (req, res, next) => {
 
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'kied34CNe3Jd208Jk', { expiresIn: '7d' });
 
-      return res.cookie('jwt', token, { httpOnly: true, sameSite: false }).send({ email });
+      return res.cookie('jwt', token, { }).send({ email });
     });
   })
   .catch(next);
